7736f5e2faf60479a4bad181716a5dc293c7f738
Luca Peduto <luca@eloquentbit.com>
2016-04-24 10:04:06 +0200

    Add Constants util class

---
+++ XComiK/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  XComiK
+//
+//  Created by Luca Peduto on 24/04/16.
+//  Copyright © 2016 Eloquent Bit. All rights reserved.
+//
+
+import Foundation
+
+typealias JSONDictionary = [String: AnyObject]
+typealias JSONArray = Array<AnyObject>
